630fa414b5fc52dd796958d82c8d1e4a5429eda8
Katumbela <ja3328173@gmail.com>
2024-07-24 16:35:28 +0100

    enable cross and install helmet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
- 
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -7,9 +7,13 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  
-  app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
