40f1b04f89b8e36c4786c4a32cc40728aee02a89
Katumbela <ja3328173@gmail.com>
2024-07-24 16:38:52 +0100

    chore: add Cross-Site Request Forgery (CSRF)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as csurf from 'csurf';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +16,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
+  app.use(csurf());
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
 }
